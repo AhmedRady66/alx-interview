3f5286063b6da8ac8c7313bb77ab5028ab1cf0a6
Ahmed Rady <rady20991@gmail.com>
2024-10-11 04:29:49 +0300

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+"""
+Solution to lockboxes problem
+"""
 
 
 def canUnlockAll(boxes):
-    """Determines if all the boxes can be opened"""
-    if type(boxes) is not list:
+    """
+    Determines whether a series of locked boxes can be opened
+    based on keys that can be attained.
+    Solution to the lockboxes problem
+    """
+    if (type(boxes)) is not list:
         return False
     elif (len(boxes)) == 0:
         return False
+
     for k in range(1, len(boxes) - 1):
         boxes_checked = False
         for idx in range(len(boxes)):
