96e6d941eae9f604e5a187a30599bf96c395b4be
Ahmed Rady <rady20991@gmail.com>
2024-10-11 04:18:07 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened."""
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+
+    return all(opened)
