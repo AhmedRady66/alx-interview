e701b80cbfa8681bb0423777ef347028f552ba0d
Ahmed Rady <rady20991@gmail.com>
2024-11-28 19:21:42 +0200

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Given a pile of coins of different values"""
+
+
+def makeChange(coins, total):
+    """Determine the fewest number of coins needed to meet a given amount"""
+    if total == 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
